17bbcf56739341a0c3c3846ac405b584de884b4c
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-07-16 17:32:03 +0600

    Updated change mode code

---
+++ Runtime/Core/VideoSettingsManager.cs
@@ -0,0 +1,51 @@
+
+using Studio23.SS2.Settings.Video.Core;
+using Studio23.SS2.Settings.Video.Data;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Studio23.SS2.Settings.Video.Core
+{
+    public class VideoSettingsManager : MonoBehaviour
+    {
+        public static VideoSettingsManager Instance; 
+        public DisplayController DisplayController;
+        public GraphicsController GraphicsController;
+
+        [SerializeField] private Volume _globalVolume;
+        [SerializeField] private GraphicsConfigurationBase _postProcessConfiguration;
+        [SerializeField] private GraphicsConfigurationBase _defaultPostProcessConfiguration;
+
+        public VideoSettingsData DefaultVideoSettingsData;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else 
+                Destroy(this);
+        }
+
+
+        public void Initialize()
+        {
+            if(_globalVolume == null)
+                _globalVolume = GameObject.FindGameObjectWithTag("GlobalVolume").GetComponent<Volume>();
+
+            _defaultPostProcessConfiguration = Resources.Load<GraphicsConfigurationBase>("Settings/Video/PostProcess/DummyPostProcess");
+            _postProcessConfiguration = Resources.Load<GraphicsConfigurationBase>("Settings/Video/PostProcess/PostProcess");
+
+            _postProcessConfiguration = _postProcessConfiguration == null
+                ? _defaultPostProcessConfiguration
+                : _postProcessConfiguration;
+
+            DisplayController.Initialize(_postProcessConfiguration);
+            GraphicsController.Initialize(_postProcessConfiguration);
+            _postProcessConfiguration.Initialize(_globalVolume);
+        }
+    }
+}
